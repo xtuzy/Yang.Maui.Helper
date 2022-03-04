36b6e171a7a3186571216776e47b4f11233da58d
杨 周 <1570571499@qq.com>
2022-03-04 14:39:54 +0800

    add default test project

---
+++ Test/Yang.Maui.Helper.Test/Platforms/Android/MainApplication.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Runtime;
+using System;
+
+namespace Yang.Maui.Helper.Test
+{
+    [Application]
+    public class MainApplication : Application
+    {
+        protected MainApplication(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
+        {
+        }
+
+        public override void OnCreate()
+        {
+            base.OnCreate();
+        }
+    }
+}
