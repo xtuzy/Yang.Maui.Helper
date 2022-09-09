5c96034d3758c15a32f96ef69e59c20f4237ddbc
yang zhou <xtuzhouyang@outlook.com>
2022-09-09 10:54:24 +0800

    add sksize exteniosn

---
+++ Test/Yang.Maui.Helper.Maui.Test/SkiaExtensionTests/ISkiaDrawable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Yang.Maui.Helper.SkiaExtension;
+
+namespace Yang.Maui.Helper.Maui.Test.SkiaExtensionTests
+{
+    internal interface ISkiaDrawable
+    {
+        void OnDraw(AndroidCanvas canvas);
+    }
+}
